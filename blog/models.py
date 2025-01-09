46f62b0bf678309a1d77496faa41c952d67e95a5
aleksandr <alexvic.che@gmail.com>
2025-01-09 16:31:47 +0300

    add friendly search url-adresses

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.db import models
+from django.urls import reverse
 from django.utils import timezone
 
 
@@ -8,8 +9,6 @@ class PublishedManager(models.Manager):
         return (
             super().get_queryset().filter(status=Post.Status.PUBLISHED)
         )
-
-
 
 
 class Post(models.Model):
@@ -24,7 +23,9 @@ class Post(models.Model):
         PUBLISHED = 'PB', 'Published'
 
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250)
+    slug = models.SlugField(max_length=250,
+                            unique_for_date='publish'
+                            )
     author = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
@@ -48,4 +49,11 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[self.publish.year,
+                                                 self.publish.month,
+                                                 self.publish.day,
+                                                 self.slug
+                                                 ])
 
--- blog/views.py
+++ blog/views.py
@@ -8,10 +8,13 @@ def post_list(request):
     posts = Post.published.all()
     return render(request, 'blog/post/list.html', {'posts': posts})
 
-def post_detail(request, id):
+def post_detail(request, year, month, day, post):
     post = get_object_or_404(
         Post,
-        id=id, 
-        status=Post.Status.PUBLISHED
+        status=Post.Status.PUBLISHED,
+        publish__year = year,
+        publish__month = month,
+        publish__day = day,
+        slug=post
     )
     return render(request, 'blog/post/detail.html', {'post': post})
