a85a1eb7a194470ac500932ea5deba36ac45cbdc
aleksandr <alexvic.che@gmail.com>
2025-01-10 11:36:55 +0300

    add forms

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,11 @@
+
+
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.http import Http404
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView
 
+from .forms import EmailPostForm
 from .models import Post
 
 
@@ -28,3 +32,21 @@ def post_detail(request, year, month, day, post):
         slug=post
     )
     return render(request, 'blog/post/detail.html', {'post': post})
+
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
+
+def post_share(request, post_id):
+    post = get_object_or_404(Post, pk=post_id, status=Post.Status.PUBLISHED)
+    if request.method == 'POST':
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+
+    else:
+        form = EmailPostForm()
+    return render(request, 'blog/post/share.html', {'post': post, 'form': form})
